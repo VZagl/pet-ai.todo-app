78e2fa30a0df4c73d9bb4ba05aa0ac642ac5371a
Zahliada VM <waah777@gmail.com>
2025-12-30 16:28:23 +0200

    feat(todo): добавить компоненты TodoInput, TodoList и TodoFooter
    - создать TodoInput с валидацией пустого ввода
    - добавить обработку Enter для быстрого добавления задач
    - реализовать TodoList с отображением отфильтрованных задач
    - добавить сообщение для пустого состояния списка
    - создать TodoFooter со счетчиком активных задач
    - интегрировать TodoFilter в TodoFooter
    - применить адаптивные стили для всех компонентов
    - покрыть все компоненты integration тестами

---
+++ src/components/TodoList/TodoList.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import type { Todo } from '../../types/todo';
+import { TodoItem } from '../TodoItem/TodoItem';
+import './TodoList.css';
+
+interface TodoListProps {
+	/** Массив задач для отображения */
+	todos: Todo[];
+	/** Обработчик переключения статуса задачи */
+	onToggle: (id: string) => void;
+	/** Обработчик удаления задачи */
+	onDelete: (id: string) => void;
+}
+
+/**
+ * Компонент списка задач
+ * Отображает список TodoItem или сообщение о пустом списке
+ */
+export const TodoList = ({ todos, onToggle, onDelete }: TodoListProps) => {
+	if (todos.length === 0) {
+		return (
+			<div className="todo-list-empty">
+				<p className="todo-list-empty__message">Нет задач для отображения</p>
+			</div>
+		);
+	}
+
+	return (
+		<ul className="todo-list">
+			{todos.map((todo) => (
+				<TodoItem
+					key={todo.id}
+					todo={todo}
+					onToggle={onToggle}
+					onDelete={onDelete}
+				/>
+			))}
+		</ul>
+	);
+};

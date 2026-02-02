0a427308bfc97992c16d68e545ab31c7b26a4f9b
Zahliada VM <waah777@gmail.com>
2026-02-02 10:32:47 +0200

    docs(todo-context): расширить JSDoc хука useTodoContext
    - расширен комментарий хука: назначение, использование внутри TodoProvider, возвращаемое значение
    - добавлена директива @returns с перечислением полей контекста
    - сохранена директива @throws для вызова вне провайдера

--- src/hooks/use-todo-context.ts
+++ src/hooks/use-todo-context.ts
@@ -5,7 +5,9 @@ import { TodoContext } from '../contexts/todo-context';
 const TODO_PROVIDER_ERROR = 'useTodoContext must be used within TodoProvider';
 
 /**
- * Хук доступа к значению контекста TODO (состояние и действия из useTodos).
+ * Хук доступа к контексту задач: список задач, счётчик активных и действия (добавить, переключить, удалить, обновить).
+ * Использовать только внутри дерева, обёрнутого в TodoProvider; вне провайдера вызов приведёт к ошибке.
+ * @returns значение контекста (todos, activeCount, addTodo, toggleTodo, deleteTodo, updateTodo)
  * @throws если вызван вне TodoProvider
  */
 export function useTodoContext(): i_todoContextValue {
